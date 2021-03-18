73a6c298e1a58d8476fcc6951515cc23eefe6407
imnotberg <aaron.m.weisberg@gmail.com>
2021-03-18 16:28:27 -0400

    made changes

--- ods/ncaab/models.py
+++ ods/ncaab/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+from picklefield.fields import PickledObjectField
 from sportsreference.ncaab.boxscore import Boxscores,Boxscore
 from datetime import datetime, timedelta
 from lxml import html
@@ -9,6 +11,65 @@ def get_value(value):
 		return value[0]
 	except:
 		return None
+class Pick:
+	def __init__(self,user,date,week,league,value,game,team,opp,line,total):
+		self._user = user
+		self._league = league
+		self._game = game
+		self._value = value
+		self._team = team
+		self._opp = opp
+		self._line = line
+		self._total = total
+		self.pick_time = datetime.now().strftime("%Y%m%d%H%M%S")
+		self.result = None
+		self.account = None
+		self.all = {f"{self.user}{self.game}":{x:y for x,y in self.__dict__.items()}}
+
+		def save_to_picks(self):
+			picks,created = Picks.objects.get_or_create(pk=1)
+			picks.picks.append(self.all)
+			picks.save()
+		save_to_picks(self)
+
+	@property
+	def user(self):
+		return self._user
+
+	@property
+	def league(self):
+		return self._league
+	@property
+	def game(self):
+		return self._game
+	@property
+	def value(self):
+		return self._value
+	@property
+	def team(self):
+		return self._team
+	@property
+	def opp(self):
+		return self._opp
+	@property
+	def line(self):
+		return self._line
+	@property
+	def total(self):
+		return self._total	 
+
+class Picks(models.Model):
+	picks = PickledObjectField(null=True)
+class Profile(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f"{self.user.username}"
+
+	def get_absolute_url(self):
+		return reverse(':profile_detail',kwargs={'pk':self.pk})
+
+
 class Team(models.Model):
 	name = models.CharField(max_length=200)
 	abbreviation = models.CharField(max_length=200)
